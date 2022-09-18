18e782f2ff66b4b2c24957ef1f86c3465e93fdfe
AntonUsov <anton.usov@stm-labs.ru>
2022-09-18 15:22:45 +0300

    Initial Commit

---
+++ Forecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Forecast
+//
+//  Created by Антон Усов on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
